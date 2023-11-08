38ea0e76484a6f6c1878a52ec5b150cd47bf674a
thallyslemos <thallysvilemos@gmail.com>
2023-11-08 10:54:53 -0300

    exclusão de descrição da lista e inclusão de botões

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,6 +9,7 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  IconButton,
   Typography,
 } from "@material-tailwind/react";
 import TodoItem from "./TodoItem";
@@ -19,7 +20,8 @@ const todos = [
     id: 1,
     title: "teste",
     completed: true,
-    description: "testando todolist e todo itemns testando todolist e todo itemns testando todolist e todo itemns",
+    description:
+      "testando todolist e todo itemns testando todolist e todo itemns testando todolist e todo itemns",
     dateLimit: "2023-10-10",
   },
   {
@@ -28,7 +30,6 @@ const todos = [
     completed: true,
     description: "testando todolist e todo itemns",
     dateLimit: "2023-12-10",
-
   },
   {
     id: 3,
@@ -39,30 +40,16 @@ const todos = [
 ];
 
 const TodoList = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleOpen = () => setOpen(!open);
 
   return (
-    <Card className="mt-6 w-96 h-fit pt-2">
-      <CardHeader className="">
-        <Accordion open={open}>
-          <AccordionHeader
-            onClick={() => handleOpen()}
-            className="justify-center text-secondary hover:text-primary hover:bg-secondary"
-          >
-            <Typography variant="h5" className="text-center">
-              TODO
-            </Typography>{" "}
-          </AccordionHeader>
-          <AccordionBody className="bg-white">
-            <Typography className=" text-primary px-2">
-              The place is close to Barceloneta Beach and bus stop just 2 min by
-              walk and near to &quot;Naviglio&quot; where you can enjoy the main
-              night life in Barcelona.
-            </Typography>
-          </AccordionBody>
-        </Accordion>
+    <Card className="mt-6 w-96 h-fit pt-2 my-auto shadow-lg bg-blue-gray-50">
+      <CardHeader className="justify-center text-white bg-primary text-secondary">
+        <Typography variant="h5" className="text-center">
+          TODO
+        </Typography>{" "}
       </CardHeader>
       <CardBody className="overflow-y-auto max-h-full">
         <section className="flex flex-col my-3 gap-2 ">
@@ -71,8 +58,39 @@ const TodoList = () => {
           ))}
         </section>
       </CardBody>
-      <CardFooter className="pt-0">
-        <Button>Read More</Button>
+      <CardFooter className="pt-0 flex gap-2">
+        <IconButton size="sm" color="green">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </IconButton>
+        <IconButton size="sm" color="red">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        </IconButton>
       </CardFooter>
     </Card>
   );
