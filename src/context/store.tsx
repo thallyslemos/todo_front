6d693e3b287a88556e8c76c24390d5058c474eb6
thallyslemos <thallysvilemos@gmail.com>
2023-11-13 00:08:09 -0300

    criação de formulario de login e cadastro

--- src/context/store.tsx
+++ src/context/store.tsx
@@ -14,11 +14,21 @@ type ToastData = {
   type: string;
 };
 
+type UserData = {
+  id: number;
+  name: string | null;
+  email: string;
+};
+
 interface ContextProps {
   toastData: ToastData;
   setToastData: Dispatch<SetStateAction<ToastData>>;
   showToast: boolean;
   setShowToast: Dispatch<SetStateAction<boolean>>;
+  userData: UserData;
+  setUserData: Dispatch<SetStateAction<UserData>>;
+  token: string;
+  setToken: Dispatch<SetStateAction<string>>;
 }
 
 const GlobalContext = createContext<ContextProps>({
@@ -26,6 +36,10 @@ const GlobalContext = createContext<ContextProps>({
   setToastData: () => {},
   showToast: false,
   setShowToast: () => {},
+  userData: { id: 0, name: null, email: "" },
+  setUserData: () => {},
+  token: "",
+  setToken: () => {},
 });
 
 export const GlobalProvider = ({ children }: { children: ReactNode }) => {
@@ -34,10 +48,25 @@ export const GlobalProvider = ({ children }: { children: ReactNode }) => {
     type: "",
   });
   const [showToast, setShowToast] = useState(false);
+  const [userData, setUserData] = useState<UserData>({
+    id: 0,
+    name: null,
+    email: "",
+  });
+  const [token, setToken] = useState("");
 
   return (
     <GlobalContext.Provider
-      value={{ toastData, setToastData, showToast, setShowToast }}
+      value={{
+        toastData,
+        setToastData,
+        showToast,
+        setShowToast,
+        userData,
+        setUserData,
+        token,
+        setToken,
+      }}
     >
       {children}
     </GlobalContext.Provider>
