e374c1f30121f84c57c71aa9ad166a26fc1ee09c
thallyslemos <thallysvilemos@gmail.com>
2023-11-14 10:07:22 -0300

    fix base url

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,4 +1,4 @@
-const baseURL = process.env.NEXT_PUBLIC_API_URLn || "http://localhost:3000";
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 function getToken() {
   if (typeof window !== "undefined") {
